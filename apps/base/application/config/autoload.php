f91691127f867c22d9032b63300cfa8473d05d26
Xavier <xperez@debian83.BEAR>
2016-02-16 18:12:56 +0100

    Changes: modules names must to be lowercase

--- apps/base/application/config/autoload.php
+++ apps/base/application/config/autoload.php
@@ -73,7 +73,7 @@ $autoload['libraries'] = array();
 |
 |	$autoload['drivers'] = array('cache');
 */
-$autoload['drivers'] = array();
+$autoload['drivers'] = array('session');
 
 /*
 | -------------------------------------------------------------------
