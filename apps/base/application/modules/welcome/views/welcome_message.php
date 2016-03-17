cbde8f535398c645fd10910962c6a56281282fdf
Xavier Perez <xmadmax@gmail.com>
2016-03-17 15:41:52 +0100

    Update version in welcome

--- apps/base/application/modules/welcome/views/welcome_message.php
+++ apps/base/application/modules/welcome/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter 3.0.4 XHMVC</title>
+	<title>Welcome to CodeIgniter <?php echo CI_VERSION;?> XHMVC</title>
 
 	<style type="text/css">
 
