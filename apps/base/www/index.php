bff8b086303f7976645f72c64c5abb853f8f659c
Xavier Perez <xmadmax@gmail.com>
2017-01-11 12:47:44 +0100

    Codeginiter v.3.1.3

--- apps/base/www/index.php
+++ apps/base/www/index.php
@@ -98,7 +98,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = '../../../core/system310';
+	$system_path = '../../../core/system313';
 
 /*
  *---------------------------------------------------------------
